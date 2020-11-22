b74b5fa2eb0791c7ee7afb0b75c0b81bd2b1f2dc
akshaywakhare <akshaywakhare661@gmail.com>
2020-11-23 01:33:36 +0530

    todo and calendar

--- app/src/main/java/com/example/track_trigger/MainActivity.java
+++ app/src/main/java/com/example/track_trigger/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.track_trigger;
 
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton clearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clearButton = (FloatingActionButton) findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clearDialog();
+            }
+        });
     }
+    private void clearDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("title");
+        alert.setMessage("Do you want to delete the entire task list?\n");
+
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                new ToDoActivity();
+            }
+        });
+
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+        alert.show();
+    }
+
 }
